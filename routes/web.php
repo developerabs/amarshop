f09b5845ce64a37165489fb4271c26e88efeaf28
developerabs <bokkor3256@gmail.com>
2026-07-19 17:36:38 +0600

    cache clear route added

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('storagelink', function () {
     Artisan::call('storage:link');
 });
+
+Route::get('cache-clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    return "Cache is cleared";
+});
