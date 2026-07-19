775b2c94c3135d0eb3521e58abb43c95843735eb
developerabs <bokkor3256@gmail.com>
2026-07-19 17:06:10 +0600

    storage route added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
 })->middleware('admin');
+Route::get('storagelink', function () {
+    Artisan::call('storage:link');
+});
