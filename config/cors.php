89e331afd0db74c247709d4c664aa040af34c9e7
developerabs <bokkor3256@gmail.com>
2026-07-19 17:40:21 +0600

    url update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://amarshop-9j3y.onrender.com'],
+    'allowed_origins' => ['https://amarshop-reactjs.vercel.app', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
